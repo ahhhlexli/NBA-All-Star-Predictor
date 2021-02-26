244f381c362f298bf9dd20ad7954e0949e8ba638
ahhhlexli <alexli0605@gmail.com>
2021-02-26 21:52:12 +0800

    Completed Player Stat Scrape
    All in one url and stat scrape.
    Output to csv.

--- Player Info.py
+++ Player Info.py
@@ -1,45 +1,81 @@
-from selenium import webdriver 
+from requests_futures.sessions import FuturesSession
+from bs4 import BeautifulSoup as soup
+import numpy as np 
 import time
+import requests
 import string
-from Individual_Player_Scrape import individual_scrape
+import csv
+
 start = time.time()
 
+def make_request(session, url):
+    future = session.get(url)
+    return future
 
-DRIVER_PATH = './chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-link_list = []
+url_list = []
 
 for letter in list(string.ascii_lowercase):
 
-    url = 'https://www.basketball-reference.com/players/' + letter
-    driver.get(url)
-
-    try:
-        table = driver.find_elements_by_xpath('//*[@id="players"]/tbody/tr')
-
-        for row in range(len(table)):
-            
-            try:
-                start_year = int(table[row].find_element_by_xpath('./td[1]').text)
-                name = table[row].find_element_by_xpath('./th').text
-                link = table[row].find_element_by_xpath('./th//a')
-
-                if start_year >= 1996:
-
-                    player_info = [name, str(link.get_attribute('href'))]
-                    link_list.append(player_info)           
-            except:
-                pass
-
-    except:
+    if letter == 'x':
         pass
+    else:
+        url = 'https://www.basketball-reference.com/players/' + letter
+        url_list.append(url)
 
+#print(url_list)
 
-for player in link_list:
+session = FuturesSession(max_workers=32)
+futures = np.array([make_request(session, url) for url in url_list])
+results = [future.result().text for future in futures]
+
+player_list = []
+
+for i in range(len(results)):
+
+    body = soup(results[i], 'html.parser').tbody
+    players = body.find_all('tr')
     
-    individual_scrape(player, driver)
+    for player in players:
+        
+        name = player.find('a').text
+        start_year = int(player.find('td', {'data-stat': 'year_min'}).text)
+        url = 'https://www.basketball-reference.com' + player.find('a')['href']
 
+        if start_year >= 1996:
+            player_list.append([name, url])
+
+print('Player List & URL Collected')
+print("Elapsed Time: %s" % (time.time() - start))
+
+session = FuturesSession(max_workers=100)
+futures = np.array([make_request(session, url[1]) for url in player_list ])
+results = [future.result().text for future in futures]
+
+print('Player Stats Collected:')
+print("Elapsed Time: %s" % (time.time() - start))
+print('Processing Stats:')
+
+for i in range(len(results)):
+
+    script = soup(results[i], 'html.parser')
+    name = script.find('h1', {'itemprop': 'name'}).span.text
+    body = script.tbody
+
+    season_totals = [[name] + [cat.text for cat in row.find_all('td')] for row in body.find_all('tr')]
+
+    temp = []
+    for season in season_totals:
+        if 'Did' in season[3]:
+            temp.append(season)
+    for i in temp:
+        season_totals.remove(i)
+
+    with open('players.csv', 'a', newline='', encoding='utf-8') as file:
+        write = csv.writer(file)
+        write.writerows(season_totals)
+    
+print('Stats written to CSV')
 print("Elapsed Time: %s" % (time.time() - start))
 
 
